fc2194e1852464c8e85c3542f5b60110ca94fd66
allenwa2_cisco <allenwa2@cisco.com>
2024-09-25 10:55:33 -0400

    Added jenkinsfile

--- src/calculator.py
+++ src/calculator.py
@@ -14,3 +14,22 @@ class Calculator:
         else:
             return "Error: Division by zero"
         
+    def square(self, num):
+        return num * num
+    
+    #add a power function to the calculation
+    def power(self, base, exponent):
+        return base ** exponent
+    
+    #add a factorial function to the calculation
+    def factorial(self, num):
+        if num < 0:
+            return "Error: Factorial is not defined for negative numbers"
+        elif num == 0:
+            return 1
+        else:
+            result = 1
+            for i in range(1, num + 1):
+                result *= i
+            return result
+        
